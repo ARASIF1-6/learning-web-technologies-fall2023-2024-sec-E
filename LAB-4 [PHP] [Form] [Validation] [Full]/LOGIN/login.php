046c4941388000fd9ccecd448f798ad83158a236
MD Aminur Rahman Asif <141429059+ARASIF1-6@users.noreply.github.com>
2023-10-14 13:29:18 +0600

    Add files via upload

---
+++ LAB-4 [PHP] [Form] [Validation] [Full]/LOGIN/login.php
@@ -0,0 +1,14 @@
+<?php 
+    $username = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
+    
+    if (strlen($username) >= 2 && preg_match('/^[a-zA-Z0-9._-]+$/', $username) && strlen($password) >= 8 && preg_match('/[@#$%]/', $password)) {
+        echo "Valid username: " . $username;
+        echo "<br>Valid password";
+    }
+
+    else {
+        echo "Invalid username/password";
+    }
+?>
+
