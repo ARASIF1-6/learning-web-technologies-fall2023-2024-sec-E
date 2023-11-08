b6b12146fe2ee64b28a6fc1238e12203b530ab89
MD Aminur Rahman Asif <141429059+ARASIF1-6@users.noreply.github.com>
2023-11-08 23:37:55 +0600

    Add files via upload

---
+++ LAB-7.1 [HTML] [JS]/Gender/gender.js
@@ -0,0 +1,16 @@
+function validgender(){
+    let gender = document.getElementsByName("gender");
+    let flag = false;
+    for (let option of gender){
+        if (option.checked){
+            flag = true;
+            break;
+        }
+    }
+    if (flag){
+        alert("option selected.");
+    }
+    else{
+        alert("please select a option!");
+    }
+}
