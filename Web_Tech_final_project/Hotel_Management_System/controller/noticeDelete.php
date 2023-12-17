484d8d885c304d5ff127d16a5358566c8b53b6ea
MD Aminur Rahman Asif <141429059+ARASIF1-6@users.noreply.github.com>
2023-12-17 23:22:19 +0600

    Delete Web_Tech_final_project/Hotel_Management_System directory

--- Web_Tech_final_project/Hotel_Management_System/controller/noticeDelete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('../model/noticeModel.php');
-    if(isset($_REQUEST['id'])){
-        $id = $_REQUEST['id'];
-        deletenotice($id);
-        header('location: ../view/write_notice.php');
-    }
-?>
